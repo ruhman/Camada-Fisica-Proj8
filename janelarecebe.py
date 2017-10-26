9d6b4d5078a26a03376a38eb2ae80c0c4d586b5e
pedrodelapena <pedrodelapena@uol.com.br>
2017-10-25 23:43:48 -0200

    front finalizado?

--- janelarecebe.py
+++ janelarecebe.py
@@ -4,21 +4,22 @@ class InterfaceTKRecebe:
 
     def __init__(self):
         self.window = tk.Tk()
+        self.input = tk.StringVar()
+        
 
-        self.input_user = tk.StringVar()
-
-        self.frame = tk.Frame(self.window, width=300, height=300)
+        self.frame = tk.Frame(self.window, height=300, width=300)
         self.frame.pack_propagate(False)
         self.frame.pack()
 
 
-    def putText(self, texto):
-        self.label = tk.Label(self.frame, text=texto)
-        self.input_user.set("")
+    def read(self, text):
+        self.label = tk.Label(self.frame, text=text)
+        self.input.set("")
         self.label.pack()
+
 
     def run(self):
         self.window.mainloop()
 
-aaa = InterfaceTKRecebe()
-aaa.run()
+interfacer = InterfaceTKRecebe()
+interfacer.run()
